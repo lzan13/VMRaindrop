c02125aa0f1a4091ee3b67db98e03e9d1b32749f
liulizheng <liulizheng@weidian.com>
2019-01-31 19:14:25 +0800

    编译最新代码

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var uglify = require('gulp-uglify');
 
 //编译、合并、压缩less文件
 gulp.task('less', function () {
-    gulp.src(['src/assets/less/**/main.less', 'src/assets/less/**/*.less'])
+    return gulp.src(['src/assets/less/**/main.less', 'src/assets/less/**/*.less'])
         .pipe(concat('style.less'))
         .pipe(less())
         .pipe(autoprefixer({
@@ -21,7 +21,7 @@ gulp.task('less', function () {
 
 //合并压缩后的css文件
 gulp.task('css', function () {
-    gulp.src(['src/assets/css/*.css'])
+    return gulp.src(['src/assets/css/*.css'])
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'Android >= 4.0'],
@@ -34,7 +34,7 @@ gulp.task('css', function () {
 
 //压缩、合并js文件
 gulp.task('main:js', function () {
-    gulp.src(['src/assets/js/jquery.min.js', 'src/assets/js/bootstrap.min.js', 'src/assets/js/pace.min.js',
+    return gulp.src(['src/assets/js/jquery.min.js', 'src/assets/js/bootstrap.min.js', 'src/assets/js/pace.min.js',
         'src/assets/js/main.js'])
         .pipe(uglify())
         .pipe(concat('index.js'))
@@ -43,7 +43,7 @@ gulp.task('main:js', function () {
 });
 
 gulp.task('post:js', function () {
-    gulp.src(['src/assets/js/highlight.pack.js', 'src/assets/js/toc.js',
+    return gulp.src(['src/assets/js/highlight.pack.js', 'src/assets/js/toc.js',
         'src/assets/js/clipboard.min.js', 'src/assets/js/post.js'])
         .pipe(uglify())
         .pipe(concat('post.js'))
@@ -53,20 +53,20 @@ gulp.task('post:js', function () {
 
 
 gulp.task('watch_less', function () {
-    gulp.watch('src/assets/less/**/*.less', ['less', 'css'])
+    return gulp.watch('src/assets/less/**/*.less', ['less', 'css'])
 });
 
 gulp.task('watch_css', function () {
-    gulp.watch('src/assets/css/*.css', ['css'])
+    return gulp.watch('src/assets/css/*.css', ['css'])
 });
 
 gulp.task('watch_js', function () {
-    gulp.watch('src/assets/js/**/*.js', ['main:js', 'post:js'])
+    return gulp.watch('src/assets/js/**/*.js', ['main:js', 'post:js'])
 });
 
 
 //文件监听
-gulp.task('auto', ['watch_less', 'watch_css', 'watch_js']);
+gulp.task('auto', gulp.series('watch_less', 'watch_css', 'watch_js'));
 
 //生产环境
-gulp.task('build', ['less', 'css', 'main:js', 'post:js']);
+gulp.task('build', gulp.series('less', 'css', 'main:js', 'post:js'));
