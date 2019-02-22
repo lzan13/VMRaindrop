084fd2891086115abe606401d5c867e0bbc254f2
liulizheng <liulizheng@weidian.com>
2019-02-22 18:00:14 +0800

    修改主题加载随机封面代码

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ VMRaindrop
 
 一个 Ghost 自用主题
 
+
+### 配置
+然后进入`Ghost`后台 > `Code injection`在`Site Header`中插入以下代码：
+```
+<script>
+    var enableRandomCover = true; //是否开启随机封面图，默认为true
+</script>
+```
+
 ### 更新
 
 - v1.1.0 修改使用 typo.css 作为文章默认样式模板
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,22 @@
 $(function () {
-    //随机显示博客封面
-    if (Maple.enableRandomCover) {
+    // 加载博客后端配置的属性
+    VBlog.enableRandomCover = typeof enableRandomCover !== 'undefined' ? enableRandomCover : true;
+
+    // 随机显示博客封面
+    if (VBlog.enableRandomCover) {
         randomSiteCover();
     }
 
-    //点击向下的箭头滚动到文章区域
+    // 点击向下的箭头滚动到文章区域
     $('.indicator').click(function () {
         var scrollTop = $('.site-content').offset().top;
-        $('html,body').animate({scrollTop: scrollTop}, 500);
+        $('html,body').animate({ scrollTop: scrollTop }, 500);
     });
 
-    //默认滚动到文章列表
+    // 默认滚动到文章列表
     if ($('body').hasClass('paged')) {
         var scrollTop = $('.site-content').offset().top;
-        $('html,body').animate({scrollTop: scrollTop}, 0);
+        $('html,body').animate({ scrollTop: scrollTop }, 0);
         $('.header-big').removeClass('invisible');
     }
 });
@@ -24,5 +27,10 @@ $(function () {
  */
 function randomSiteCover() {
     var randomNum = Math.floor(Math.random() * 9 + 1);
-    $('.header-big').css('background-image', "url(" + Maple.blogUrl + "/assets/images/covers/cover-bg-" + randomNum + ".jpg)");
+    // 这里先获取下要显示封面图部分的背景图是否已经有设置，如果已经设置，则不再设置随机背景图
+    var cover = $(".header-big").css("backgroundImage");
+    cover = cover.split("(\"")[1].split("\")")[0];
+    if(cover === VBlog.url + "/"){
+        $('.header-big').css('background-image', "url(" + VBlog.url + "/assets/images/covers/cover-bg-" + randomNum + ".jpg)");
+    }
 }
